d218c312f66b0d2cddd4ff80ac5e269da11dfa3f
Nayel22 <nayelsolorzanom213@gmail.com>
2025-01-27 21:16:52 -0600

    Trabajo conexion

---
+++ Models/ContactMessagescs.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebFormContact.Models
+{
+    public class ContactMessagescs
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [EmailAddress(ErrorMessage = "El correo no puede exceder los 75 caracteres")]
+        [StringLength(75, ErrorMessage = "El correo no puede exceder los 75 caracteres")]
+        public string Correo { get; set; }
+        
+
+        [Required(ErrorMessage = "El Mensaje es obligatorio")]
+        [StringLength(150, ErrorMessage = "El mensaje no puede exceder los 150 caracteres")]
+        public string Mensaje { get; set; }
+       
+
+    }
+}
